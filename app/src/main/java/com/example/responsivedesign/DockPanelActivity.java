58a24cf129e5daa3f137644efba20ccb665dd892
Uchchash Barua <shrabon56@gmail.com>
2019-11-10 02:47:39 +0600

    Uchchash

---
+++ app/src/main/java/com/example/responsivedesign/DockPanelActivity.java
@@ -0,0 +1,12 @@
+package com.example.responsivedesign;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DockPanelActivity extends  AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dock_panel);
+    }
+}
